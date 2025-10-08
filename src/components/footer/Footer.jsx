c60e1a623a1be3acc054f466e0b093b08246080b
xasanov274 <xasanovabdulaziz51@gmail.com>
2025-10-08 14:30:06 +0500

    asvdasdvs

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import "./Footer.css";
 function Footer() {
-  return
-   <>
-<footer>
+  return (
+    <footer>
       <div className="contaier">
         <div className="footer-top">
           <img className="img" src="public/Group 111 (1).png" alt="" />
           <div className="divider"></div>
-          <div className="text">Virtual Class <br/> for Zoom</div>
+          <div className="text">
+            Virtual Class <br /> for Zoom
+          </div>
         </div>
 
         <div className="orta">
@@ -18,21 +19,26 @@ function Footer() {
             <button className="btn">Subscribe</button>
           </div>
         </div>
-        
 
         <div className="pas">
           <ul>
-            <li><a href="#">Careers</a></li>
+            <li>
+              <a href="#">Careers</a>
+            </li>
             <span class="spa">|</span>
-            <li><a href="#">Privacy Policy</a></li>
+            <li>
+              <a href="#">Privacy Policy</a>
+            </li>
             <span class="spa">|</span>
-            <li><a href="#"> Terms & Conditions</a></li>
+            <li>
+              <a href="#"> Terms & Conditions</a>
+            </li>
           </ul>
         </div>
         <p className="pde">© 2021 Class Technologies Inc. </p>
       </div>
     </footer>
-  </>;
+  );
 }
 
 export default Footer;
