2b06101a21824130d029b03dd6ac6a7cba220d77
xasanov274 <xasanovabdulaziz51@gmail.com>
2025-10-08 14:27:10 +0500

    SCAS

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,39 @@
 import React from "react";
 import "./Footer.css";
 function Footer() {
-  return <div>Footer</div>;
+  return
+   <>
+   <footer>
+    <div className="contaier">
+      <div className="footer-top">
+        <img src="public/rasm1.png" alt="" />
+        <div className="divider">|</div>
+        <div className="text">
+          Virtual Class for Zoom
+        </div>
+
+        <div className="orta">
+          <h3>Subscribe to get our Newsletter</h3>
+          <div className="inbt">
+            <input type="text" placeholder="Your Email" />
+            <button>Subscribe</button>
+          </div>
+        </div>
+
+
+        <div className="pas">
+          <ul>
+            <li><a href="">Careers</a></li>
+            <span>|</span>
+            <li><a href="">Privacy Policy</a></li>
+            <span>|</span>
+            <li><a href="">Terms & Conditions</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
+  </>;
 }
 
 export default Footer;
