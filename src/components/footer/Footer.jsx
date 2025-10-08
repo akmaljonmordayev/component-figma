9b2b7985b4577907aeb45068fc6fb11a029cc67a
xasanov274 <xasanovabdulaziz51@gmail.com>
2025-10-08 14:27:27 +0500

    asvdasdv

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,36 +3,35 @@ import "./Footer.css";
 function Footer() {
   return
    <>
-   <footer>
-    <div className="contaier">
-      <div className="footer-top">
-        <img src="public/rasm1.png" alt="" />
-        <div className="divider">|</div>
-        <div className="text">
-          Virtual Class for Zoom
+<footer>
+      <div className="contaier">
+        <div className="footer-top">
+          <img className="img" src="public/Group 111 (1).png" alt="" />
+          <div className="divider"></div>
+          <div className="text">Virtual Class <br/> for Zoom</div>
         </div>
 
         <div className="orta">
-          <h3>Subscribe to get our Newsletter</h3>
+          <h3 className="h3">Subscribe to get our Newsletter</h3>
           <div className="inbt">
-            <input type="text" placeholder="Your Email" />
-            <button>Subscribe</button>
+            <input className="in" type="text" placeholder="Your Email" />
+            <button className="btn">Subscribe</button>
           </div>
         </div>
-
+        
 
         <div className="pas">
           <ul>
-            <li><a href="">Careers</a></li>
-            <span>|</span>
-            <li><a href="">Privacy Policy</a></li>
-            <span>|</span>
-            <li><a href="">Terms & Conditions</a></li>
+            <li><a href="#">Careers</a></li>
+            <span class="spa">|</span>
+            <li><a href="#">Privacy Policy</a></li>
+            <span class="spa">|</span>
+            <li><a href="#"> Terms & Conditions</a></li>
           </ul>
         </div>
+        <p className="pde">© 2021 Class Technologies Inc. </p>
       </div>
-    </div>
-  </footer>
+    </footer>
   </>;
 }
 
